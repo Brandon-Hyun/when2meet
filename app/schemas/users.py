05fcf67a2dda024c5a20e345b2e882533b15367b
RandomUsername_Kr <theoriginaldongik@gmail.com>
2025-12-30 13:53:28 +0900

    <style> black .

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,19 +1,21 @@
 from pydantic import BaseModel
 from enum import Enum
 
+
 # 성별은 male | female // Enumrate 둘 중 하나여야 한다고 정의함.
 class Gender(str, Enum):
     male = "male"
     female = "female"
-    
+
+
 # 유저 생성시 pydantic 검증
 class UserCreate(BaseModel):
     username: str
     age: int
-    gender: Gender      # 클래스 Gender  enum규칙에 따름.. 상속이 파이썬의 꽃이라는게 이런건가;
-    
+    gender: Gender  # 클래스 Gender  enum규칙에 따름.. 상속이 파이썬의 꽃이라는게 이런건가;
+
+
 class UserUpdate(BaseModel):
     username: str | None = None
     age: int | None = None
     gender: Gender | None = None
-    
--- temp.py
+++ temp.py
@@ -1,16 +1,16 @@
 # my_int: int = "1"
-my_int : int = 123 # Literal : 값, inference (Literal 기반으로 추론한다.)  // 근데 왜 
+my_int: int = 123  # Literal : 값, inference (Literal 기반으로 추론한다.)  // 근데 왜
 
 # dict, set, list, tuple -> collection
 # int, str, bool 같은 자료형의 자료를 담기위한 collection
 
-my_list : list[str] = ["abc", "def" ]
-my_list2= ["abc", "def"]                # mypy 가 추론할 수 있으면 넘어감
+my_list: list[str] = ["abc", "def"]
+my_list2 = ["abc", "def"]  # mypy 가 추론할 수 있으면 넘어감
 
-#immutable (생성 후 길이 변경할 수 없다.)
+# immutable (생성 후 길이 변경할 수 없다.)
 # my_tuple: tuple[str,str] = ("str", "str", "str")  #튜플의 길이 명시함,
-#길이를 모르는 경우는?
-my_tuple: tuple[str,...] = ("str", "str", "str")  # ...
+# 길이를 모르는 경우는?
+my_tuple: tuple[str, ...] = ("str", "str", "str")  # ...
 
 # pipeline | 은 or 의미함       // bool 은 int의 서브클래스
 
@@ -20,9 +20,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
